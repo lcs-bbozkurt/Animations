dc5948d033b1f3ba3d0698e0d24a922ff0479484
Berk Bozkurt <BBozkurt@lcs.on.ca>
2022-04-27 11:55:09 -0400

    The unfilled star dissapears and the field star appears.

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -14,34 +14,73 @@ struct ContentView: View {
     @State var starRotation = 0.0
     
     // 1. Initial opacity for unfilled star
-    
     @State var unfilledStarOpacity = 1.0 // Opaque
+    
+    
+    // 1. Initial opacity for filled star
+    @State var filledStarOpacity = 0.0 // See-thorugh
+    
     
     // MARK: Computed Properties
     
     
     var body: some View {
-        Image(systemName: "star")
-            .resizable()
-            .frame(width: 40, height: 40)
-            .foregroundColor(.yellow)
-            .rotation3DEffect(.degrees(starRotation), axis: (x: 0, y: 1, z: 0))
-            .opacity(unfilledStarOpacity)
         
-        // 2. Trigger the stage change with a tap.
-            .onTapGesture {
-                withAnimation (
-                    Animation.easeInOut(duration: 2.0)
-                ) {
-                    // 3. Rotate the star (change rotation state)
-                    starRotation += 360.0 * 2
+        ZStack {
+            
+            
+            // Filled Star
+            Image(systemName: "star.fill")
+                .resizable()
+                .frame(width: 40, height: 40)
+                .foregroundColor(.yellow)
+                .rotation3DEffect(.degrees(starRotation), axis: (x: 0, y: 1, z: 0))
+                .opacity(filledStarOpacity)
+            
+            // 2. Trigger the stage change with a tap.
+                .onTapGesture {
+                    withAnimation (
+                        Animation.easeInOut(duration: 2.0)
+                    ) {
+                        // 3. Rotate the star (change rotation state)
+                        starRotation += 360.0 * 2      // 3. Desired end opacity for unfilled star
+                       
+                        unfilledStarOpacity += 0.0 // See-through
+                        
+                        
+                        // 3. Desired end opacity for filled star
+                        filledStarOpacity = 1.0 // See-through
+
+                        
+                        
                     
-                    
-                    // 3. Desired end opacity for unfilled star
-                    unfilledStarOpacity += 0.0 // See through
-                    
+                }
+            }
+            
+            
+            // Unfilled star
+            Image(systemName: "star")
+                .resizable()
+                .frame(width: 40, height: 40)
+                .foregroundColor(.yellow)
+                .rotation3DEffect(.degrees(starRotation), axis: (x: 0, y: 1, z: 0))
+                .opacity(unfilledStarOpacity)
+
+            // 2. Trigger the stage change with a tap.
+                .onTapGesture {
+                    withAnimation (
+                        Animation.easeInOut(duration: 2.0)
+                    ) {
+                        // 3. Rotate the star (change rotation state)
+                        starRotation += 360.0 * 2
+
+
+                  
+                }
             }
         }
+        
+        
     }
 }
 
